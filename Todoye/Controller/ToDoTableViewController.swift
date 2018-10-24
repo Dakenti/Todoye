2ce126fed1f8d503fb6609bd08ba22bd90fe6bed
Dake <didar3@mail..ru>
2018-10-24 08:25:44 +0530

    implementation of CRUD (create, read, update, delete) in CoreData

--- Todoye/Controller/ToDoTableViewController.swift
+++ Todoye/Controller/ToDoTableViewController.swift
@@ -7,42 +7,19 @@
 //
 
 import UIKit
+import CoreData
 
 class ToDoTableViewController: UITableViewController {
     
     let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     
     var itemArray = [Item]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
         loadData()
-    }
-    
-    // MARK: - Persistant data save, retrieve
-    
-    func loadData(){
-        if let data = try? Data(contentsOf: dataFilePath!) {
-            let decoder = PropertyListDecoder()
-            do{
-                itemArray = try decoder.decode([Item].self, from: data)
-            } catch {
-                print("failed to retrice")
-            }
-        }
-    }
-    
-    func save(){
-        let encoder = PropertyListEncoder()
-        
-        do{
-            let data = try encoder.encode(itemArray)
-            try data.write(to: dataFilePath!)
-        } catch {
-            print("failed to save")
-        }
-        tableView.reloadData()
     }
     
     // MARK: - Add NavBar Button Configuration
@@ -51,8 +28,9 @@ class ToDoTableViewController: UITableViewController {
         var textField = UITextField()
         let alert = UIAlertController(title: "Add New ToDoye Item", message: "", preferredStyle: .alert)
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
-            let item = Item()
+            let item = Item(context: self.context)
             item.title = textField.text!
+            item.done = false
             self.itemArray.append(item)
             self.save()
         }
@@ -88,6 +66,10 @@ class ToDoTableViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        ---------deletion of an element---------
+//        context.delete(itemArray[indexPath.row])
+//        itemArray.remove(at: indexPath.row)
+        
         itemArray[indexPath.row].done = !itemArray[indexPath.row].done
         
         save()
@@ -95,49 +77,58 @@ class ToDoTableViewController: UITableViewController {
         tableView.deselectRow(at: indexPath, animated: true)
     }
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
+
+// MARK: - Persistant data save, retrieve
+
+extension ToDoTableViewController{
+    
+    func save(){
+        do{
+            try context.save()
+        } catch {
+            print("Failed to save in SQLite")
+        }
+        tableView.reloadData()
+    }
+    
+    func loadData(){
+        let request: NSFetchRequest<Item> = Item.fetchRequest()
+        do{
+            itemArray = try context.fetch(request)
+        } catch {
+            print("failure in reading data \(error)")
+        }
+    }
+    //    func loadData(){
+    //        if let data = try? Data(contentsOf: dataFilePath!) {
+    //            let decoder = PropertyListDecoder()
+    //            do{
+    //                itemArray = try decoder.decode([Item].self, from: data)
+    //            } catch {
+    //                print("failed to retrice")
+    //            }
+    //        }
+    //    }
+    //
+    //    func save(){
+    //        let encoder = PropertyListEncoder()
+    //
+    //        do{
+    //            let data = try encoder.encode(itemArray)
+    //            try data.write(to: dataFilePath!)
+    //        } catch {
+    //            print("failed to save")
+    //        }
+    //        tableView.reloadData()
+    //    }
+}
+
+
+
+
+
+
+
+
+
---
+++ Todoye/Model/Item+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Item+CoreDataClass.swift
+//  Todoye
+//
+//  Created by Dake Aga on 10/23/18.
+//  Copyright © 2018 Dake Aga. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+
+public class Item: NSManagedObject {
+
+}
